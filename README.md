3aaef405f5a59b4506aafc1b3249f0f9674ecc0e
iruslanalexan <iruslanalexan@gmail.com>
2022-10-21 08:22:11 +0300

    vue js setup

---
+++ README.md
@@ -0,0 +1,12 @@
+### Установка
+
+* cp .env.example .env
+
+* composer install
+
+* php artisan key:generate
+
+* npm install
+
+* npm run production
+
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,32 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css';
 
 window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue comptwonts. It will recursively scan this directory for the Vue
- * comptwonts and automatically register them with their "basename".
- *
- * Eg. ./comptwonts/ExampleComptwont.vue -> <example-comptwont></example-comptwont>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.comptwont(key.split('/').pop().split('.')[0], files(key).default))
+Vue.use(VueRouter)
+Vue.use(Vuesax)
 
 Vue.comptwont('example-comptwont', require('./comptwonts/ExampleComptwont.vue').default);
+Vue.comptwont('navbar', require('./comptwonts/NavbarComptwont.vue').default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding comptwonts to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', comptwont: Foo },
+  { path: '/bar', comptwont: Bar }
+]
+
+const router = new VueRouter({
+   routes: routes,
+   created : function(){
+       alert("vue router is being created");
+   }
+  
+})
 
 const app = new Vue({
-    el: '#app',
-});
+  router
+}).$mount('#app')
