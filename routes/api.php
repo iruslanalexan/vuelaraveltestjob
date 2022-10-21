8d9d9f2ef43ff1422959621d4ad401f581c47766
iruslanalexan <iruslanalexan@gmail.com>
2022-10-21 12:09:11 +0300

    rw routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,53 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/cars/get_image', function (Request $request) {
+    $car = \App\Car::findOrFail($request->id);
+    return response()->file(storage_path('app/'.$car->image));
+    
+});
+
+Route::get('/colors', function (Request $request) {
+    return \App\CarColor::all();
+});
+
+Route::get('/cars/delete', function (Request $request) {
+    \App\Car::destroy($request->id);
+    return ["status" => $request->id];
+});
+
+Route::get('/cars', function (Request $request) {
+    $d = \App\Car::with('colors')->get();
+    // dd($d);
+    return $d;
+});
+
+Route::post('/cars/create', function (Request $request) {
+    // $request->validate([
+        // 'user_name'  => 'required',
+        // 'user_image' => 'required|image|max:2048'
+    // ]);    
+    $file = $request->file('image');
+    $path = $file->path();
+    $imagePath = $file->store('images');
+    
+    $car = new \App\Car;
+    $car->name = $request->name;
+    $car->pub_year = $request->pub_year;
+    $car->engine_volume = $request->engine_volume;
+    $car->engine_number = $request->engine_number;
+
+    $car->description = $request->description;
+
+    $car->image = $imagePath;
+    $car->save();
+
+    $colors = explode(",",$request->colors);
+    $car->colors()->sync($colors);
+
+    return ['success' => true];
+});
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,3 @@ Route::get('/{vue_capture?}', function () {
 })->where('vue_capture', '[\/\w\.-]*');
 
 Auth::routes();
-
-
-Route::get('/home', 'HomeController@index')->name('home');
